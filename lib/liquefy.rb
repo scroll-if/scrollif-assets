455b6b646c10eea6c11a799c2ee6d94ccc477e23
blakedavies <blake@scrollif.com.au>
2020-03-03 20:32:54 +1000

    Update liquefy.rb

--- lib/liquefy.rb
+++ lib/liquefy.rb
@@ -2,11 +2,12 @@
 # https://stackoverflow.com/questions/39858593/how-to-manually-process-liquid-tags-in-jekyll
 
 module Jekyll
-    module LiquefyFilter
-        def liquefy(input)
-            Liquid::Template.parse(input).render(@context)
+    module LiquifyFilter
+        def liquify(input)
+            output = Liquid::Template.parse(input)
+            output.render(@context)
         end
     end
 end
 
-Liquid::Template.register_filter(Jekyll::LiquefyFilter)
+Liquid::Template.register_filter(Jekyll::LiquifyFilter)
